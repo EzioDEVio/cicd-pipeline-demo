7b1d8a9949d8d396bc6431f082739a11f4f31e71
Mohammed Aldaraji <71232730+theslash84@users.noreply.github.com>
2024-03-29 09:46:50 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,23 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_vpc" "existing_vpc" {
+# using existng vpc
+data "aws_vpc" "existing_vpc" {   
   id = var.vpc_id
 }
 
+# using existng subnet
 data "aws_subnet" "existing_subnet" {
   id = var.subnet_id
 }
 
-# Retrieve the private key from AWS Secrets Manager using the variable
+# It is very importan to use AWS secret manager to store secrets and sensetives for optimal security settings. 
+# we will etrieve the private key from AWS Secrets Manager using the variable
+
 data "aws_secretsmanager_secret" "cicd_private_key" {
   name = var.secret_name
 }
+
 
 data "aws_secretsmanager_secret_version" "cicd_private_key_version" {
   secret_id = data.aws_secretsmanager_secret.cicd_private_key.id
@@ -61,29 +66,29 @@ resource "aws_instance" "web_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = data.aws_subnet.existing_subnet.id
-  key_name               = var.key_name
+  key_name               = var.key_name               ### I created a key pair in aws console and named it CICDdemoKey
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "CICD-Web-Instance"
   }
-
+#here we refrence the private key from AWS secret manager 
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = data.aws_secretsmanager_secret_version.cicd_private_key_version.secret_string
+    private_key = data.aws_secretsmanager_secret_version.cicd_private_key_version.secret_string  
     host        = self.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
       "sudo yum update -y",
-      "sudo yum install -y docker",
+      "sudo yum install -y docker",                     ## here we are deploying docker image(built and pushed to GHCR) 
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
       "sudo usermod -aG docker ec2-user",
       "sudo docker pull ghcr.io/eziodevio/ghcr-democicdapp:v.1.1.1",
-      "sudo docker run -d -p 80:80 ghcr.io/eziodevio/ghcr-democicdapp:v.1.1.1"
+      "sudo docker run -d -p 80:80 ghcr.io/eziodevio/ghcr-democicdapp:v.1.1.1"             #the name of docker image we are deploying...
     ]
   }
 }
