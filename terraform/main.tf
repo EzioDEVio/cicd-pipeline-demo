f9000c0adb936f640e58fb58def39aa04de926d6
Mohammed Aldaraji <71232730+theslash84@users.noreply.github.com>
2024-03-30 01:08:57 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,7 +87,7 @@ resource "null_resource" "docker_image_update" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = local.private_key
+    private_key = file("${path.module}/CICDKey.pem")
     host        = aws_instance.web_instance.public_ip
   }
 
