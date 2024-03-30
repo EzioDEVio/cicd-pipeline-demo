8a7a2042e4f2876a57f66cc7287d35f4b73f275f
Mohammed Aldaraji <71232730+theslash84@users.noreply.github.com>
2024-03-29 22:09:55 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -53,6 +53,6 @@ variable "docker_password" {
 variable "docker_image_tag" {
   description = "The tag of the Docker image to deploy"
   type        = string
-  default     = "latest" # Default can be set to "latest" or any stable version you prefer
 }
+
 
