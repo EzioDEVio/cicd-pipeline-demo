18e94fb37e6c93e01051d2c19866203e5aaf1995
Mohammed Aldaraji <71232730+theslash84@users.noreply.github.com>
2024-03-29 20:46:23 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,23 +2,17 @@ provider "aws" {
   region = var.region
 }
 
-# using existng vpc
 data "aws_vpc" "existing_vpc" {
   id = var.vpc_id
 }
 
-# using existng subnet
 data "aws_subnet" "existing_subnet" {
   id = var.subnet_id
 }
 
-# It is very importan to use AWS secret manager to store secrets and sensetives for optimal security settings. 
-# we will etrieve the private key from AWS Secrets Manager using the variable
-
 data "aws_secretsmanager_secret" "cicd_private_key" {
   name = var.secret_name
 }
-
 
 data "aws_secretsmanager_secret_version" "cicd_private_key_version" {
   secret_id = data.aws_secretsmanager_secret.cicd_private_key.id
@@ -66,13 +60,13 @@ resource "aws_instance" "web_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = data.aws_subnet.existing_subnet.id
-  key_name               = var.key_name ### I created a key pair in aws console and named it CICDdemoKey
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "CICD-Web-Instance"
   }
-  #here we refrence the private key from AWS secret manager 
+
   connection {
     type        = "ssh"
     user        = "ec2-user"
@@ -83,12 +77,13 @@ resource "aws_instance" "web_instance" {
   provisioner "remote-exec" {
     inline = [
       "sudo yum update -y",
-      "sudo yum install -y docker", ## here we are deploying docker image(built and pushed to GHCR) 
+      "sudo yum install -y docker",
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
       "sudo usermod -aG docker ec2-user",
-      "sudo docker pull ghcr.io/eziodevio/ghcr-democicdapp:v.1.2.1",
-      "sudo docker run -d -p 80:80 ghcr.io/eziodevio/ghcr-democicdapp:v.1.2.1" #the name of docker image we are deploying...
+      // Use the Docker image tag variable
+      "sudo docker pull ghcr.io/eziodevio/ghcr-democicdapp:${var.docker_image_tag}",
+      "sudo docker run -d -p 80:80 ghcr.io/eziodevio/ghcr-democicdapp:${var.docker_image_tag}"
     ]
   }
 }
