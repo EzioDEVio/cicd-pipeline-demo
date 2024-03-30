b30c9b0091ddeabf514e5fb21c9ba56379d17448
Mohammed Aldaraji <71232730+theslash84@users.noreply.github.com>
2024-03-30 01:31:33 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,7 +87,7 @@ resource "null_resource" "docker_image_update" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = aws_secretsmanager_secret_version.private_key.secret_string
+    private_key = local.private_key
     host        = aws_instance.web_instance.public_ip
   }
 
