327f78f7d9e3759ca7cbf139dafc27f8c2950308
Mohammed Aldaraji <71232730+theslash84@users.noreply.github.com>
2024-03-30 17:17:54 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,33 +113,24 @@ resource "aws_instance" "web_instance" {
   iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
 
   user_data = <<-EOF
-                #!/bin/bash
-                # Update the installed packages and package cache
-                sudo yum update -y
+              #!/bin/bash
+              # Install Docker
+              sudo dnf install docker -y
+              
+              # Start and enable Docker service
+              sudo systemctl start docker
+              sudo systemctl enable docker
 
-                # Install Docker
-                sudo amazon-linux-extras install docker -y
+              # Add 'ec2-user' to the 'docker' group
+              sudo usermod -a -G docker ec2-user
 
-                # Start and enable Docker service
-                sudo systemctl start docker
-                sudo systemctl enable docker
-
-                # Ensure the Docker service starts on reboot
-                sudo chkconfig docker on
-
-                # Add 'ec2-user' to the 'docker' group to execute Docker commands without using 'sudo'
-                sudo usermod -a -G docker ec2-user
-
-                # Docker post-installation steps recommend logging out and back in to re-evaluate group membership
-                # We can work around this by applying permissions and running a new shell instance as ec2-user
-
-                # Pull and run the Docker image as ec2-user
-                echo "Pulling new image with tag: ${var.docker_image_tag}"
-                sudo -u ec2-user -i docker pull ghcr.io/${var.repo_owner}/ghcr-democicdapp:${var.docker_image_tag}
-                sudo -u ec2-user -i docker stop web_container || true
-                sudo -u ec2-user -i docker rm web_container || true
-                sudo -u ec2-user -i docker run -d --name web_container -p 80:80 ghcr.io/${var.repo_owner}/ghcr-democicdapp:${var.docker_image_tag}
-                EOF
+              # Pull and run the Docker image
+              echo "Pulling new image with tag: ${var.docker_image_tag}"
+              sudo docker pull ghcr.io/${var.repo_owner}/ghcr-democicdapp:${var.docker_image_tag}
+              sudo docker stop web_container || true
+              sudo docker rm web_container || true
+              sudo docker run -d --name web_container -p 80:80 ghcr.io/${var.repo_owner}/ghcr-democicdapp:${var.docker_image_tag}
+              EOF
 
   tags = {
     Name = "CICD-Web-Instance"
