da1eacf6b6eb3f977e688dc126370345ffe6248f
Mohammed Aldaraji <71232730+theslash84@users.noreply.github.com>
2024-03-30 00:27:24 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,3 +15,8 @@ output "web_instance_public_ip" {
 output "web_security_group_id" {
   value = aws_security_group.web_sg.id
 }
+
+output "private_key_sample" {
+  value     = substr(data.aws_secretsmanager_secret_version.cicd_private_key_version.secret_string, 0, 30)
+  sensitive = true
+}
